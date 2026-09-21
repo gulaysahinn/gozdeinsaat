e94b6e38a50e994523f2886e3693c57d39cf5f71
Gulay Sahin <sahinngulayy12@gmail.com>
2026-09-21 14:52:58 +0300

    hata giderildi

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter basename="/gozdeinsaat" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </HelmetProvider>
